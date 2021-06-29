7f50087da970f7f78df06d72bc1325320481fa66
xiao <xiao@168.com>
2021-06-29 23:02:08 +0800

    master push commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello hello Git5");
         System.out.println("hello hello Git6");
         System.out.println("hello pull test");
+        System.out.println("hello push test");
     }
 }
